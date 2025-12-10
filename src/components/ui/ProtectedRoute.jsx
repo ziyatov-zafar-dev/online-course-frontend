21300d439935c76d0200d924d51871dbb6d57098
ziyatov-zafar-dev <zziyatov811@gmail.com>
2025-12-11 04:02:02 +0500

    sd

--- src/components/ui/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-export default function ProtectedRoute() {
-	const token = localStorage.getItem("access_token")
-	return token ? <Outlet /> : <Navigate to='/auth/sign-in' replace />
-}
